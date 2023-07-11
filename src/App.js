a494ba00193615151c34ba099933c0a49dfd3700
loveshsingh <loveshsinghsuperfine@gmail.com>
2023-07-11 12:20:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card_container">
+        <div style={{ width: "30%", padding: '1rem' }}>
+          <h1>Why the industry chooses clickdee</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a 
+          </p>
+        </div>
+        <Card heading={"Heading"} desc={"This is desc"} />
+        <Card heading={"Heading"} desc={"This is desc"} />
+        <Card heading={"Heading"} desc={"This is desc"} />
+        <Card heading={"Heading"} desc={"This is desc"} />
+        <Card heading={"Heading"} desc={"This is desc"} />
+      </div>
     </div>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './card.css'
+import MaterialIcon from 'material-icons-react';
 
-function Card() {
+function Card({heading, desc}) {
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
-    <div>Card</div>
+    <div className='card' onMouseOver={handleMouseOver}
+    onMouseOut={handleMouseOut}>
+      <div className='card__content'>
+      <MaterialIcon icon="dashboard" />
+      <h1 className='card__heading'>{heading}</h1>
+      {isHovering && <p>{desc}</p>}
+      </div>
+    </div>
   )
 }
 
